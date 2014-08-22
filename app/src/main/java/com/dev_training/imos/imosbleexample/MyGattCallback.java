a24e0409e06bf24bb48c30ec0bade33719ad1dfb
imos-android <imos@dev-training.com>
2014-08-22 14:35:30 +0900

    update

--- app/src/main/java/com/dev_training/imos/imosbleexample/MyGattCallback.java
+++ app/src/main/java/com/dev_training/imos/imosbleexample/MyGattCallback.java
@@ -9,10 +9,17 @@ import android.bluetooth.BluetoothProfile;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.UUID;
+
 /**
  * Created by i-MOS on 2014/08/22.
  */
 public class MyGattCallback extends BluetoothGattCallback{
+    String TI_BASE_UUID = "-0451-4000-b000-000000000000";
+    UUID accServiceUuid = UUID.fromString("f000aa10"+TI_BASE_UUID);
+    UUID accCharacteristicUuid = UUID.fromString("f000aa11"+TI_BASE_UUID);
+    UUID accConfigCharacteristicUuid = UUID.fromString("f000aa12"+TI_BASE_UUID);
+    UUID accPeriodCharacteristicUuid = UUID.fromString("f000aa13"+TI_BASE_UUID);
 
     private Activity mActivity;
 
@@ -65,8 +72,15 @@ public class MyGattCallback extends BluetoothGattCallback{
         mActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                TextView buttonStateTextView = (TextView) mActivity.findViewById(R.id.buttonStateTextView);
-                buttonStateTextView.setText("Simple Key State: "+characteristic.getValue()[0]);
+                if (characteristic.getUuid().equals(accCharacteristicUuid)){
+                    TextView accStateTextView = (TextView) mActivity.findViewById(R.id.accTextView);
+                    byte accValue[] = characteristic.getValue();
+                    accStateTextView.setText("Acc State: "+accValue[0]+","+accValue[1]+","+accValue[2]);
+                } else {
+                    TextView buttonStateTextView = (TextView) mActivity.findViewById(R.id.buttonStateTextView);
+                    buttonStateTextView.setText("Simple Key State: "+characteristic.getValue()[0]);
+                }
+
             }
         });
     }
