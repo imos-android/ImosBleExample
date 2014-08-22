731348c60713a70a59e53ca40eecb4420b0c01bc
imos-android <imos@dev-training.com>
2014-08-22 13:07:55 +0900

    2nd commit

--- app/src/main/java/com/dev_training/imos/imosbleexample/MyActivity.java
+++ app/src/main/java/com/dev_training/imos/imosbleexample/MyActivity.java
@@ -3,6 +3,15 @@ package com.dev_training.imos.imosbleexample;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +19,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import static  com.dev_training.imos.imosbleexample.UUIDs.*;
 
 
 
@@ -49,7 +59,13 @@ public class MyActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+        private static final String TAG = "PlaceholderFragment";
+        private static final String MY_DEVICE_ADDRESS ="BC:6A:29:AB:5C:C7" ;
+        private View mRootView;
+        private BluetoothAdapter mBluetoothAdapter;
+        private MyGattCallback mMyGattCallback;
+        private BluetoothGatt mGatt;
 
         public PlaceholderFragment() {
         }
@@ -57,8 +73,56 @@ public class MyActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-            return rootView;
+            mRootView = inflater.inflate(R.layout.fragment_my, container, false);
+            mRootView.findViewById(R.id.button).setOnClickListener(this);
+            mRootView.findViewById(R.id.enableNotifyButton).setOnClickListener(this);
+            return mRootView;
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.button:
+                    BluetoothManager bluetoothManager = (BluetoothManager) getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
+                    mBluetoothAdapter = bluetoothManager.getAdapter();
+                    if (mBluetoothAdapter == null
+                            || !mBluetoothAdapter.isEnabled()){
+                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivity(intent);
+                        return;
+                    }
+                    mBluetoothAdapter.startLeScan(new BluetoothAdapter.LeScanCallback() {
+                        @Override
+                        public void onLeScan(BluetoothDevice bluetoothDevice, int i, byte[] bytes) {
+                            // Scan
+                            if (bluetoothDevice.getAddress().equals(MY_DEVICE_ADDRESS)){
+                                //
+                                mMyGattCallback = new MyGattCallback(getActivity());
+                                mGatt = bluetoothDevice.connectGatt(
+                                        getActivity(),
+                                        false,
+                                        mMyGattCallback
+                                );
+                            }
+                        }
+                    });
+                    break;
+                case R.id.enableNotifyButton:
+                    BluetoothGattService simpleKeyService
+                            = mGatt.getService(UUID_TI_SENSOR_TAG_SIMPLE_KEY_SERVICE);
+                    BluetoothGattCharacteristic simpleKeyServiceCharacteristic
+                            = simpleKeyService.getCharacteristic(UUID_TI_SENSOR_TAG_SIMPLE_KEY);
+
+                    mGatt.setCharacteristicNotification(simpleKeyServiceCharacteristic,
+                            true);
+                    BluetoothGattDescriptor descriptor
+                            = simpleKeyServiceCharacteristic.getDescriptor(
+                            UUID_CLIENT_CHARACTERISTIC_CONFIGURATION);
+                    descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    mGatt.writeDescriptor(descriptor);
+
+                    break;
+            }
         }
     }
 }
---
+++ app/src/main/java/com/dev_training/imos/imosbleexample/MyGattCallback.java
@@ -0,0 +1,73 @@
+package com.dev_training.imos.imosbleexample;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothProfile;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by i-MOS on 2014/08/22.
+ */
+public class MyGattCallback extends BluetoothGattCallback{
+
+    private Activity mActivity;
+
+    public MyGattCallback(Activity mActivity) {
+        this.mActivity = mActivity;
+    }
+
+    @Override
+    public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+        super.onConnectionStateChange(gatt, status, newState);
+        if (status == BluetoothGatt.GATT_SUCCESS){
+            if (newState == BluetoothProfile.STATE_CONNECTED){
+                mActivity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(mActivity,
+                                "接続に成功しました。", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            gatt.discoverServices();
+        } else {
+
+        }
+    }
+
+    @Override
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+        super.onServicesDiscovered(gatt, status);
+        //
+
+    }
+
+    @Override
+    public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+        super.onDescriptorWrite(gatt, descriptor, status);
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(mActivity,
+                        "通知成功",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onCharacteristicChanged(BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic) {
+        super.onCharacteristicChanged(gatt, characteristic);
+
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView buttonStateTextView = (TextView) mActivity.findViewById(R.id.buttonStateTextView);
+                buttonStateTextView.setText(""+characteristic.getValue()[0]);
+            }
+        });
+    }
+}
